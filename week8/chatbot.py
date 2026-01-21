b3deae5ff96b78a6d41ce604acd8148b5ac82c30
Vanij Prasher <2004vanij.prasher@gmail.com>
2026-01-21 18:15:13 +0530

    update langchain

--- week8/chatbot.py
+++ week8/chatbot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import CSVLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
